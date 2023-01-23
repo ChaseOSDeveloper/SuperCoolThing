1e103fd83194e143265365aadd535ef00356ba70
ChaseOSDeveloper <90874854+ChaseOSDeveloper@users.noreply.github.com>
2023-01-22 19:13:41 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,7 @@
 local hook
 hook = hookfunction(game.HttpGet,function(self,url,...)
     if tostring(url) == "https://raw.githubusercontent.com/StenDirt/Trash-Game/main/UI.lua" then
-        return hook(self,"https://raw.githubusercontent.com/VertigoCool99/Script/main/BuildABoat/BackupLib.lua",...)
+        return hook(self,"https://raw.githubusercontent.com/ChaseOSDeveloper/SuperCoolThing/main/uilib.lua",...)
     end
     return hook(self,url,...)
 end)
